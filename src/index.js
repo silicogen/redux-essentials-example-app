68795a224ec2221dc63b62d2d2f4ab6d8be356ba
alex <alexey.chuprov@gmail.com>
2021-12-29 08:47:34 +0300

    fetchUsers

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css'
 import App from './App'
 import store from './app/store'
 import { Provider } from 'react-redux'
-
+import { fetchUsers } from './features/users/usersSlice'
 import { worker } from './api/server'
 
 // Start our mock API server
 worker.start({ onUnhandledRequest: 'bypass' })
+store.dispatch(fetchUsers())
 
 ReactDOM.render(
   <React.StrictMode>
