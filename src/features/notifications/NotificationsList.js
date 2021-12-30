6cf0ccfd39ce4a796b04070e7520a9b843048980
alex <alexey.chuprov@gmail.com>
2021-12-30 11:25:40 +0300

    NotificationsList

--- src/features/notifications/NotificationsList.js
+++ src/features/notifications/NotificationsList.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from 'react'
+import { useLayoutEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { formatDistanceToNow, parseISO } from 'date-fns'
 import classnames from 'classnames'
@@ -19,33 +19,32 @@ export const NotificationsList = () => {
     dispatch(allNotificationsRead())
   })
 
-  const renderedNotifications = notifications.map((notification) => {
-    const date = parseISO(notification.date)
-    const timeAgo = formatDistanceToNow(date)
-    const user = users.find((user) => user.id === notification.user) || {
-      name: 'Unknown User',
-    }
+  const renderedNotifications = notifications
+    .map(notification => {
+      const date = parseISO(notification.date)
+      const timeAgo = formatDistanceToNow(date)
+      const user = users
+        .find(user => user.id === notification.user)
+        || { name: 'Unknown User' }
 
-    const notificationClassname = classnames('notification', {
-      new: notification.isNew,
-    })
+      const notificationClassname = classnames('notification', { new: notification.isNew })
 
-    return (
-      <div key={notification.id} className={notificationClassname}>
+      return <div
+        key={notification.id}
+        className={notificationClassname}>
         <div>
-          <b>{user.name}</b> {notification.message}
+          <b>{user.name}</b>
+          {notification.message}
         </div>
         <div title={notification.date}>
           <i>{timeAgo} ago</i>
         </div>
       </div>
-    )
-  })
+    })
 
-  return (
-    <section className="notificationsList">
-      <h2>Notifications</h2>
-      {renderedNotifications}
-    </section>
-  )
+  return <section
+    className="notificationsList">
+    <h2>Notifications</h2>
+    {renderedNotifications}
+  </section>
 }
